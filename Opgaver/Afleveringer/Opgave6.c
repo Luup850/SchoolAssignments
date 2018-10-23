a64361e4f0b7a9e56d87052ed2fc2a471d122679
Luup850 <marcusdchr@gmail.com>
2018-10-23 19:37:51 +0200

    L

---
+++ Opgaver/Afleveringer/Opgave6.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<math.h>
+
+/*
+* Prototypes
+*/
+double trap(double, double, int, double(*)(double));
+double getYCoordinate(double);
+
+/*
+*   Main function
+*/
+int main(void)
+{
+    /* Define user input variables */
+    double a, b, n;
+
+    /* Ask user for inputs */
+    printf("Please input a, b, n in the following form\n<a b n>\n");
+    scanf(" %lf %lf %lf", &a, &b, &n);
+
+    /* Print the area under the defined graph for the defined interval */
+    printf("The sum under the graph is: %lf", trap(a, b, n, getYCoordinate));
+
+    return 0;
+}
+
+/*  ----------[Function]--------------
+*   Calculate the area under the graph
+*/
+double trap(double a, double b, int n, double(*f)(double x))
+{
+    double h = (b - a) / n;
+    double sum = 0;
+
+    /* Slice the graph in to n defined of times */
+    for(int i = 0; i < n; i++)
+    {
+        /* Get the sum of all the slices */
+        sum += f(a + ((b - a) / n) * i);
+    }
+
+    /* Calculate the area and return it */
+    return (h / 2) * (f(a) + f(b) + 2 * sum);
+}
+
+/*  ---------[Function]-------------
+*   The equation that we want to use
+*/
+double getYCoordinate(double x)
+{
+    return sqrt(4-powf(x, 2));
+}
