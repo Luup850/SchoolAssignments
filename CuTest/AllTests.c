8545df583a3d918fa63321a42b111f60355bf5b3
Luup850 <marcusdchr@gmail.com>
2018-10-24 15:19:26 +0200

    Added CuTest

--- CuTest/AllTests.c
+++ CuTest/AllTests.c
@@ -1,25 +1,22 @@
-#include <stdio.h>
-
 #include "CuTest.h"
-
-CuSuite* CuGetSuite();
-CuSuite* CuStringGetSuite();
-
-void RunAllTests(void)
-{
-	CuString *output = CuStringNew();
-	CuSuite* suite = CuSuiteNew();
-
-	CuSuiteAddSuite(suite, CuGetSuite());
-	CuSuiteAddSuite(suite, CuStringGetSuite());
-
-	CuSuiteRun(suite);
-	CuSuiteSummary(suite, output);
-	CuSuiteDetails(suite, output);
-	printf("%s\n", output->buffer);
+#include <stdio.h>
+    
+CuSuite* StrUtilGetSuite();
+    
+void RunAllTests(void) {
+  CuString *output = CuStringNew();
+  CuSuite* suite = CuSuiteNew();
+     
+  // Adding test suites:
+  CuSuiteAddSuite(suite, (CuSuite*)daysInMonthGetSuite());
+  CuSuiteAddSuite(suite, (CuSuite*)isLeapYearGetSuite());  
+ 
+  CuSuiteRun(suite);
+  CuSuiteSummary(suite, output);
+  CuSuiteDetails(suite, output);
+  printf("%s\n", output->buffer);
 }
-
-int main(void)
-{
-	RunAllTests();
+    
+int main(void) {
+    RunAllTests();
 }
